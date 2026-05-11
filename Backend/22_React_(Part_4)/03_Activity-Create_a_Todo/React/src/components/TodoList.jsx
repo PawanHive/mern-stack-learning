14d5fe719260fbd61a90c00de62dd4d9e8e404a0
Pawan Yadav <hivepawan@gmail.com>
2026-05-11 18:48:32 +0530

    learn to update all element in array

--- Backend/22_React_(Part_4)/03_Activity-Create_a_Todo/React/src/components/TodoList.jsx
+++ Backend/22_React_(Part_4)/03_Activity-Create_a_Todo/React/src/components/TodoList.jsx
@@ -7,7 +7,7 @@ export default function TodoList() {
 
   let addNewTask = () => {
     setTodos((prevTodos) => {
-      return [...prevTodos, { task: newTodo, id: uuidv4() }]
+      return [...prevTodos, { task: newTodo, id: uuidv4() }];
     });
     setNewTodo(""); // this will make input empty after clicking on add task
     // console.log("we have to add new task to do")
@@ -19,17 +19,29 @@ export default function TodoList() {
     // console.log(event.target.value)
   };
 
-  // Delete TODO 
-  let deleteTodo = (id) => { 
+  // Delete Element of array
+  let deleteTodo = (id) => {
     // (it will take id as argument and find what exactly have to delete)
     // mostly filter() method used to delete element from array in react and this method always return new of copy of array
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id != id))
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id != id));
 
     // let copy = todos.filter((todo) => todo.id != id)
     // console.log(copy)
     // console.log(id)
     // console.log("Task to be deleted")
-  }
+  };
+
+  // Update all element of Array to Uppercase
+  let upperCaseAll = () => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      });
+    });
+  };
 
   return (
     <div>
@@ -50,13 +62,18 @@ export default function TodoList() {
       <ul>
         {/* always map are used to render element from array */}
         {todos.map((todo) => {
-          return <li key={todo.id}>
-            <span>{todo.task}</span>
-            &nbsp; &nbsp;
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>;
+          return (
+            <li key={todo.id}>
+              <span>{todo.task}</span>
+              &nbsp; &nbsp;
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+          );
         })}
       </ul>
+
+      <br />
+      <button onClick={upperCaseAll}>UpperCase All</button>
     </div>
   );
 }
